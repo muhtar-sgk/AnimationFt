5e313e60061ca74fa74eed7eadbe4d2ea4a61e5a
muhtar-sgk <muhtarrn@gmail.com>
2023-07-06 15:22:50 +0700

    create page route animation

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/second_page.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
@@ -7,9 +8,29 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text("Fisrt page"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Fisrt page"),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).push(_createRoute());
+              }, 
+              child: Text("Go!")
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const SecondPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
 }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -5,8 +5,11 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Second page"),
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text("Second page"),
+      ),
     );
   }
 }
